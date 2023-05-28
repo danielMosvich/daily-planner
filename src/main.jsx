17c5bd70120a6d476884157c8539a3577ca3e7fc
danielMosvich <mrdaniraymay@gmail.com>
2023-05-28 12:33:20 -0500

    router fixed?

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home.jsx";
 const router = createBrowserRouter([
   {
@@ -13,8 +13,11 @@ const router = createBrowserRouter([
   {
     path: "/place",
     element: <Home />,
-    errorElement:<h1>home xd</h1>
   },
+  {
+    path:"*",
+    element: <Navigate to={"/"} />
+  }
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
